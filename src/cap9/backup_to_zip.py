1f4cdf7c7cc424b2ada39c8b6422f1e22d33f62c
Gabriel <gabrielcapuerj@gmail.com>
2020-06-14 21:38:45 -0300

    adicionando backup_to_zip

--- src/cap9/backup_to_zip.py
+++ src/cap9/backup_to_zip.py
@@ -3,8 +3,8 @@ import os
 
 
 def backup_to_zip(folder):
-    folder = os.path,abspath(folder)
-
+    folder = os.path.abspath(folder)
+   
     number = 1
 
     while True:
@@ -15,4 +15,22 @@ def backup_to_zip(folder):
 
         number += 1
 
-        
+    print(f'Creating {zip_file_name}...')
+    backup_zip = zipfile.ZipFile(zip_file_name, 'w')
+    print('Done')
+
+    for foldername, _, files in os.walk(folder):
+        print(f'Adding files in {foldername}')
+
+        backup_zip.write(foldername)
+
+        for file in files:
+            new_base = os.path.basename(folder) + '_'
+            if file.startswith(new_base) and file.endswith('.zip'):
+                continue
+            backup_zip.write(os.path.join(foldername, file))
+
+    backup_zip.close()
+    print('Done')
+
+backup_to_zip('./examples')
