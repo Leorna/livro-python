27abb83132a5fce659e073710ea5f02066bcc7f5
Gabriel <gabrielcapuerj@gmail.com>
2020-06-19 19:45:03 -0300

    adicionando practice no cap9

---
+++ src/cap9/practice/copy_files/__init__.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import time
+
+
+def copy_files(path, destiny,  *ends_of_file):
+    if not os.path.exists(path) or not os.path.exists(destiny):
+        print('Path does not exist!')
+        return
+
+    try:
+        print('Creating copied folder...')
+        os.makedirs(f'{destiny}/copied')
+    except:
+        print('Folder alread created')
+    else:
+        print('Folder created')
+    
+
+    time.sleep(2)
+
+    copied_folder_path = os.path.join(os.path.abspath(destiny), 'copied')
+    
+    print(f'\n----Coping files in {path} to {copied_folder_path}----\n')
+    for file in os.listdir(path):
+        if len(ends_of_file):
+            for end_of_file in ends_of_file:
+                if file.endswith(end_of_file):
+                    print(f'Coping {file}')
+                    shutil.copy(os.path.join(path, file), copied_folder_path)
+                    time.sleep(2)
+        else:
+            print(f'Coping {file}')
+            shutil.copy(os.path.join(path, file), copied_folder_path)
+            time.sleep(2)
+
+        
+    
+
+    
---
+++ src/cap9/practice/main.py
@@ -0,0 +1,9 @@
+import copy_files
+
+
+def main():
+    copy_files.copy_files('/home/gabriel/example', '/home/gabriel/Documentos', '.jpg', '.pdf')
+
+
+if __name__ == '__main__':
+    main()
